aea6795ec6a4df29461198b6714e3c3311b8a959
Alexandra <leysanspb@mail.ru>
2022-12-11 14:15:19 +0300

    Task8

---
+++ Lesson_1/Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿int b = 1;
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+for (b % 2==0; b < a; b ++)
+{
+    Console.WriteLine(b);
+    if (b == a)
+    break;
+}
