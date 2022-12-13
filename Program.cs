e090efe1e91348ed49b9d3ea9cc2adc93e3752ba
Alexandra Chirkova <93881730+AvokrichA@users.noreply.github.com>
2022-12-14 00:09:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,3 +32,32 @@ int result = 0;
     }
 return result;
 }
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine ("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Нет");
+        break;
+    case 2:
+        Console.WriteLine("Нет");
+        break;
+    case 3:
+        Console.WriteLine("Нет");
+        break;
+    case 4:
+        Console.WriteLine("Нет");
+        break;
+    case 5:
+        Console.WriteLine("Нет");
+        break;
+    case 6:
+        Console.WriteLine("Да");
+        break;
+    case 7:
+        Console.WriteLine("Да");
+        break;                                
+}
